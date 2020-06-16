8a1e5562cc8dd3b19e782a46aac91e2c6356e82a
pennqin <qinpengzzj@gmail.com>
2020-06-16 18:11:52 +0800

    Revert "DataBinding + ViewModel." This kind of use is meaningless.
    This reverts commit 1d4138918f9813b528d55659ae64e208e3eb8df5.

--- databindingdemo/src/main/java/com/example/databindingdemo/User.kt
+++ databindingdemo/src/main/java/com/example/databindingdemo/User.kt
@@ -1,5 +1,3 @@
 package com.example.databindingdemo
 
-import androidx.lifecycle.ViewModel
-
-class User(val firstName: String, val lastName: String) : ViewModel()
+data class User(val firstName: String, val lastName: String)
